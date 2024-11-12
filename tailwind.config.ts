b72d8191caa7230d54708eb95bd1b4e72cb795e3
Yoan Lin <yoanlin068@gmail.com>
2024-11-12 17:56:04 +0800

    finish configs

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,17 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        primary: "#8486a9",
+        secondary: "#fb6087",
+        neutral: {
+          100: "#ffffff",
+          200: "#343650",
+          300: "#1e1f29",
+          400: "#191a24",
+        },
+      },
+      fontFamily: {
+        sans: ["var(--font-redHat)"],
       },
     },
   },
